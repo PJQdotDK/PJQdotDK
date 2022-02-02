d38af896eb5b614e70c6322848942a4a432b8292
Pia Jønck <pjq@atp.dk>
2022-02-02 14:47:15 +0000

    Week2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Filter, FilterOperator ) {
         "use strict";
 
         return Controller.extend("dk.atp.sap.test.minapp3.controller.View1", {
 
-            onShowHello: function () { 
+            onShowHello: function () {
+//                var beskrivelse = this.getView().getModel("oModel").getProperty("/readonly/beskrivelse");
                 sap.m.MessageToast.show("This message should appear in the message toast");
+             },
+             
+             onFilterItems: function (oEvent) { 
+                var aFilter = [],
+                sQuery = oEvent.getParameter("query"),
+                oList = this.getView().byId("_idTable2"),
+                oBinding = oList.getBinding("items");
+                if (sQuery) {
+                    aFilter.push(new Filter("int_aktart", FilterOperator.Contains, sQuery));
+                }
+                oBinding.filter(aFilter);    
              },
 
         });
